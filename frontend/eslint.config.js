974cf9b0be218e2c564be3397574c91b17bb5138
i454502 <i454502@fontysict.nl>
2025-06-22 17:04:51 +0200

    test

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,11 @@ import js from "@eslint/js"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 
+// Clean up the browser globals object by trimming whitespace from all keys.
+const cleanedBrowserGlobals = Object.fromEntries(
+  Object.entries(globals.browser).map(([key, value]) => [key.trim(), value])
+)
+
 export default [
   { ignores: ["dist"] },
   {
@@ -10,7 +15,7 @@ export default [
     languageOptions: {
       ecmaVersion: 2020,
       globals: {
-        ...globals.browser,
+        ...cleanedBrowserGlobals,
       },
       parserOptions: {
         ecmaVersion: "latest",
